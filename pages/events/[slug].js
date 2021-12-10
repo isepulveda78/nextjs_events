31c02ca503c7c65a530adb24f13be7453a9a2a48
Sepulveda <sepulvi@gcsnc.com>
2021-12-10 13:52:50 -0500

    geolocation

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -7,7 +7,7 @@ import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Event.module.css'
 import { useRouter } from 'next/router'
-
+import EventMap from '@/components/EventMap'
 export default function EventPage({ evt }) {
   const router = useRouter()
   
@@ -35,7 +35,7 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
-
+          <EventMap evt={evt} />
         <Link href='/events'>
           <a className={styles.back}>{'<'} Go Back</a>
         </Link>
