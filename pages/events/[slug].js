d0c1a21bf82f33ce0bd4061742027cb5ead16679
Israel Sepulveda <sepulveda.israel@gmail.com>
2021-12-12 20:07:10 -0500

    Update [slug].js

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -4,13 +4,14 @@ import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
+import EventMap from '@/components/EventMap'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Event.module.css'
 import { useRouter } from 'next/router'
-import EventMap from '@/components/EventMap'
+
 export default function EventPage({ evt }) {
   const router = useRouter()
-  
+
   return (
     <Layout>
       <div className={styles.event}>
@@ -35,7 +36,9 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
-          <EventMap evt={evt} />
+
+        <EventMap evt={evt} />
+
         <Link href='/events'>
           <a className={styles.back}>{'<'} Go Back</a>
         </Link>
@@ -43,6 +46,32 @@ export default function EventPage({ evt }) {
     </Layout>
   )
 }
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`)
+//   const events = await res.json()
+
+//   const paths = events.map((evt) => ({
+//     params: { slug: evt.slug },
+//   }))
+
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
+
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//     revalidate: 1,
+//   }
+// }
 
 export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`)
